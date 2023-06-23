d99ad2fe2eb3e31073a3b7320fda8463b77f8b52
George Mathew Paul <125870230+GeorgeMathewPaul@users.noreply.github.com>
2023-06-23 21:19:54 +0530

    Home page basics

---
+++ android/app/src/main/kotlin/com/example/homemd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homemd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List img = [
+    "HomePage1.jpg",
+    "HomePage2.jpg",
+    "HomePage3.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: img.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("img/" + img[index]),
+                      fit: BoxFit.contain,
+                      alignment: Alignment.bottomCenter)),
+            );
+          }),
+    );
+  }
+}
